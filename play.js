28628e54707dbac099df07cef679f4f70f8bda17
AJSawatzky <andrassawatzky@gmail.com>
2022-11-16 23:31:38 -0800

    module expoting client.js to play.js

--- play.js
+++ play.js
@@ -1,18 +1,2 @@
-const net = require("net");
-const { isPromise } = require("util/types");
+const {connect} = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: IP, // IP address here,
-    port: PORT// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect()
