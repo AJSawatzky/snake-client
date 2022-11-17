c4d6260184a2352be3f8d5ac0ad1d99cf5da5df4
AJSawatzky <andrassawatzky@gmail.com>
2022-11-16 23:25:02 -0800

    snake-client -> play.js initial creation.

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require("net");
+const { isPromise } = require("util/types");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP, // IP address here,
+    port: PORT// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect()
